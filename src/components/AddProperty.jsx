99345a993a64ad81480c20ac66d39e6c2e5887d3
Ramesh Kumar Mahto <88615111+Ramesh151@users.noreply.github.com>
2024-04-06 12:05:11 +0530

    Update AddProperty.jsx

--- src/components/AddProperty.jsx
+++ src/components/AddProperty.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+
 const Add = () => {
   const [buttonDisabled, setButtonDisabled] = useState(true);
   const [loading, setLoading] = React.useState(false);
@@ -8,6 +9,7 @@ const Add = () => {
     email: "",
     phoneNumber: "",
     images: null,
+    videos: null,
   });
 
   useEffect(() => {
@@ -15,7 +17,7 @@ const Add = () => {
       formData.address.length > 0 &&
       formData.email.length > 0 &&
       formData.phoneNumber.length > 0 &&
-      formData.images !== ""
+      (formData.images !== null || formData.videos !== null)
     ) {
       setButtonDisabled(false);
     } else {
@@ -31,29 +33,44 @@ const Add = () => {
     }));
   };
 
-  const handleimagesChange = (e) => {
+  const handleImagesChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
-      images: e.target.files[0],
+      images: e.target.files,
+    }));
+  };
+
+  const handleVideosChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      videos: e.target.files,
     }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formDataObject = {
-      address: formData.address,
-      email: formData.email,
-      phoneNumber: formData.phoneNumber,
-      images: formData.images,
-    };
+    const formDataObject = new FormData();
+    formDataObject.append("address", formData.address);
+    formDataObject.append("email", formData.email);
+    formDataObject.append("phoneNumber", formData.phoneNumber);
+    if (formData.images) {
+      for (let i = 0; i < formData.images.length; i++) {
+        formDataObject.append("images", formData.images[i]);
+      }
+    }
+    if (formData.videos) {
+      for (let i = 0; i < formData.videos.length; i++) {
+        formDataObject.append("videos", formData.videos[i]);
+      }
+    }
 
     console.log(formDataObject);
 
     try {
       setLoading(true);
       const response = await axios.post(
-        "https://server-jvhw.onrender.com/api/addproperty",
-        formData,
+        "http://localhost:8000/api/addproperty",
+        formDataObject,
         {
           headers: {
             "Content-Type": "multipart/form-data",
@@ -65,15 +82,17 @@ const Add = () => {
       console.log("Property add failed", error.message);
     } finally {
       setLoading(false);
-    }
 
-    setFormData({
-      address: "",
-      email: "",
-      phoneNumber: "",
-      images: null,
-    });
+      setFormData({
+        address: "",
+        email: "",
+        phoneNumber: "",
+        images: null,
+        videos: null,
+      });
+    }
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -83,7 +102,6 @@ const Add = () => {
               {loading ? "Loading.." : "Add Property"}
             </h3>
           </div>
-
           <div className="p-6 space-y-6">
             <div className="grid grid-cols-6 gap-6">
               <div className="col-span-6 sm:col-span-3">
@@ -145,15 +163,33 @@ const Add = () => {
                   htmlFor="images"
                   className="text-sm font-medium text-gray-900 block mb-2"
                 >
-                  Property images
+                  Property Images
                 </label>
-
                 <input
                   type="file"
                   id="images"
                   name="images"
-                  accept="images/*"
-                  onChange={handleimagesChange}
+                  accept="image/*"
+                  multiple
+                  onChange={handleImagesChange}
+                  className="block w-full cursor-pointer appearance-none rounded-l-md border border-gray-200 bg-white px-3 py-2 text-sm transition focus:z-10 focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="videos"
+                  className="text-sm font-medium text-gray-900 block mb-2"
+                >
+                  Property Videos
+                </label>
+                <input
+                  type="file"
+                  id="videos"
+                  name="videos"
+                  accept="video/*"
+                  multiple
+                  onChange={handleVideosChange}
                   className="block w-full cursor-pointer appearance-none rounded-l-md border border-gray-200 bg-white px-3 py-2 text-sm transition focus:z-10 focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
                 />
               </div>
@@ -177,3 +213,4 @@ const Add = () => {
 };
 
 export default Add;
+
